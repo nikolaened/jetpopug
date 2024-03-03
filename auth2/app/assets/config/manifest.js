53ed0b5cfaa0c77091bea86e78c8a4e57d3d1755
nikolaened <nikolaenya.e.d@gmail.com>
2024-03-03 20:07:07 +0300

    AUTH: Addint doorkeeper

--- auth2/app/assets/config/manifest.js
+++ auth2/app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link doorkeeper/application.css
+//= link doorkeeper/admin/application.css
--- auth2/app/models/account.rb
+++ auth2/app/models/account.rb
@@ -4,12 +4,21 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   enum role: {
     admin: 'admin',
     accounting_clerk: 'accounting_clerk',
     repairman: 'repairman',
     employee: 'employee'
   }
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
 end
 
