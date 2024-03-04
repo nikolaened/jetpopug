d3aa7c4884553fb156b03c757f1c92cc0e85fe5a
nikolaened <nikolaenya.e.d@gmail.com>
2024-03-04 12:24:38 +0300

    Add events producing

--- auth2/app/models/account.rb
+++ auth2/app/models/account.rb
@@ -21,5 +21,15 @@ class Account < ApplicationRecord
            class_name: 'Doorkeeper::AccessToken',
            foreign_key: :resource_owner_id,
            dependent: :delete_all # or :destroy if you need callbacks
+
+  after_commit :produce_create_event, on: :create
+
+  def produce_create_event
+    event = {
+      event_name: 'AccountCreated',
+      data: attributes.except("id", "updated_at").merge(event_name: 'AccountCreated')
+    }
+    KAFKA_PRODUCER.produce_sync(topic: 'account-streaming', payload: event.to_json)
+  end
 end
 
