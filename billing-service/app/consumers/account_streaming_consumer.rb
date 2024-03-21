5329185badd82e7646480de8dc561b651d7a53c3
nikolaened <nikolaenya.e.d@gmail.com>
2024-03-21 19:01:02 +0300

    Add version check to consumers

--- billing-service/app/consumers/account_streaming_consumer.rb
+++ billing-service/app/consumers/account_streaming_consumer.rb
@@ -7,17 +7,18 @@ class AccountStreamingConsumer < ApplicationConsumer
     messages.each do |message|
       payload = message.payload
       event_name = payload["event_name"]
+      event_version = payload["event_version"]
       data = payload["data"]
 
-      case event_name
-      when "AccountCreated"
+      case [event_name, event_version]
+      when ["AccountCreated", 1], ["AccountCreated", nil]
         Account.transaction do
           account = account_relation.create!(data)
           Balance.create!(account: account)
         end
-      when "AccountUpdated"
+      when ["AccountUpdated", 1], ["AccountUpdated", nil]
         account_relation.find_or_initialize_by(public_id: data["public_id"]).update!(data.slice(*Account::ALLOWED_UPDATE_ATTRIBUTES))
-      when "AccountDeleted"
+      when ["AccountDeleted", 1], ["AccountDeleted", nil]
         account_relation.find_by_public_id(data["public_id"]).update!(active: false, disabled_at: data["disabled_at"])
       else
         puts "Unsupported event"
--- billing-service/app/consumers/task_workflow_consumer.rb
+++ billing-service/app/consumers/task_workflow_consumer.rb
@@ -7,10 +7,11 @@ class TaskWorkflowConsumer < ApplicationConsumer
       payload = message.payload
       puts payload
       event_name = payload["event_name"]
+      event_version = payload["event_version"]
       data = payload["data"]
 
-      case event_name
-      when "TaskAdded"
+      case [event_name, event_version]
+      when ["TaskAdded", 1], ["TaskAdded", nil]
         account = Account.find_by_public_id(data['public_id'])
         if account.blank?
           puts "No associated account #{data['public_id']}"
@@ -20,7 +21,7 @@ class TaskWorkflowConsumer < ApplicationConsumer
           task.save!
           SimpleBillingLogic.create_deposit_transaction(account, task.fee, task.public_id)
         end
-      when "TaskAssigned"
+      when ["TaskAssigned", 1], ["TaskAssigned", nil]
         account = Account.find_by_public_id(data['assignee_public_id'])
         if account.blank?
           puts "No associated account #{data['public_id']}"
@@ -30,7 +31,7 @@ class TaskWorkflowConsumer < ApplicationConsumer
           task.save!
           SimpleBillingLogic.create_deposit_transaction(account, task.fee, task.public_id)
         end
-      when "TaskCompleted"
+      when ["TaskCompleted", 1], ["TaskCompleted", nil]
         account = Account.find_by_public_id(data['last_assignee_public_id'])
         if account.blank?
           puts "No associated account #{data['public_id']}"
